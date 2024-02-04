46c45c2f172dcb334ef19d14a41824f5659860ab
r-sakuraba <ruki.bloom@gmail.com>
2024-02-05 04:45:47 +0900

    パル配合-順検索実装.

---
+++ src/lib/util/pal.ts
@@ -0,0 +1,14 @@
+export type Pal = {
+	id: string;
+	name: string;
+};
+
+export type PalMap = {
+	[key: string]: Pal;
+};
+
+export interface Combination {
+	[key: string]: { [key: string]: string };
+}
+
+export const getDisplayPalFullName = (pal: Pal) => `No.${pal.id} ${pal.name}`;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import type { Pal, Combination, PalMap } from '$lib/util/pal';
+	import pals from '$lib/assets/pal.json';
+	import combination from '$lib/assets/combination.json';
+	import { getDisplayPalFullName } from '$lib/util/pal';
+
+	let title = 'パル配合シミュレーション';
+	let selected: Pal;
+	const combinations = combination as Combination;
+	const palMap = pals as PalMap;
+	$: combos = selected
+		? Object.entries(combinations[selected?.name]).map(([parent2, result]) => [
+				selected,
+				palMap[parent2],
+				palMap[result]
+			])
+		: [];
+</script>
+
+<h1>{title}</h1>
+<p>{selected ? getDisplayPalFullName(selected) : ''}</p>
+<select bind:value={selected}>
+	{#each Object.values(pals) as pal}
+		<option value={pal}>{getDisplayPalFullName(pal)}</option>
+	{/each}
+</select>
+{#if selected}
+	<ul>
+		{#each combos as combo}
+			<li>{combo[0].name} × {combo[1].name} = {combo[2].name}</li>
+		{/each}
+	</ul>
+{/if}
