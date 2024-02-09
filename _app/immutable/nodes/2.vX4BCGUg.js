c4fdc9d942e421a799b2d485d9d59ebe2bb54042
r-sakuraba <ruki.bloom@gmail.com>
2024-02-10 02:51:04 +0900

    Updates

---
+++ _app/immutable/nodes/2.vX4BCGUg.js
@@ -0,0 +1,1 @@
+import{s}from"../chunks/scheduler.IoPLG4dD.js";import{S as t,i as e}from"../chunks/index.EteLzElm.js";class l extends t{constructor(o){super(),e(this,o,null,null,s,{})}}export{l as component};
