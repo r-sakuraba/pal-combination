07a6bea1f230b25987fafd626c967b8bf0efd8dc
r-sakuraba <ruki.bloom@gmail.com>
2024-02-10 20:34:11 +0900

    cloudflare用のビルド設定追加.

--- svelte.config.js
+++ svelte.config.js
@@ -1,26 +1,43 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+// import adapter from '@sveltejs/adapter-static';
+// import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-// const dev = process.argv.include('dev');
+// // const dev = process.argv.include('dev');
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+// 	// for more information about preprocessors
+// 	preprocess: vitePreprocess(),
 
+// 	kit: {
+// 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+// 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+// 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+// 		adapter: adapter({
+// 			pages: 'build',
+//             assets: 'build'
+// 		}),
+// 		paths: {
+// 			base: process.argv.includes('dev') ? '' : '/pal-combination'
+// 		}
+// 	}
+// };
+
+// export default config;
+
+
+// 以下cloudflare用
+// =======================
+import adapter from '@sveltejs/adapter-cloudflare';
+
+export default {
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			pages: 'build',
-            assets: 'build'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : '/pal-combination'
-		}
+			// See below for an explanation of these options
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			}
+		})
 	}
 };
-
-export default config;
