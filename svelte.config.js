12ec7ee85b59a1c196acdadac70fe98215555cae
r-sakuraba <ruki.bloom@gmail.com>
2024-02-10 20:47:32 +0900

    cloudflare用のビルド設定追加.

--- svelte.config.js
+++ svelte.config.js
@@ -29,8 +29,10 @@
 // 以下cloudflare用
 // =======================
 import adapter from '@sveltejs/adapter-cloudflare';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 export default {
+	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter({
 			// See below for an explanation of these options
